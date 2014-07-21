8722b668ddac3475d8e8948a7c84427734a7c02f
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2014-07-21 15:51:26 +0900

    update travis.yml and test

--- travis_build.sh
+++ travis_build.sh
@@ -5,8 +5,8 @@ echo "${xcode_workspace}"
 echo "-------------------"
 
 
-PROJECT=sampleCaptureForIos/sampleCaptureForIos.xcworkspace
-SCHEME=sampleCaptureForIosTests
+PROJECT=sampleCaptureForIos.xcworkspace
+SCHEME=sampleCaptureForIos
 TEST_SDK=iphonesimulator
 
 CONFIGURATION_DEBUG=Debug
@@ -14,9 +14,9 @@ DESTINATION="platform=iOS Simulator,name=iPhone Retina (4-inch),OS=7.1"
 
 # test コマンド - clean, build, XCTest を実行する
 xcodebuild \
-  -workspace $(PROJECT)
-  -scheme $(SCHEME) \
-  -sdk $(TEST_SDK) \
-  -configuration $(CONFIGURATION_DEBUG) \
-  -destination $(DESTINATION) \
+  -workspace "${PROJECT}" \
+  -scheme "${SCHEME}" \
+  -sdk "${TEST_SDK}" \
+  -configuration "${CONFIGURATION_DEBUG}" \
+  -destination "${DESTINATION}" \
   clean build test
